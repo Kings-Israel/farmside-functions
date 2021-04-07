142f9d91e9efb5c115cc691ead234c9d28c7a912
kings-israel <milimokings@gmail.com>
2021-04-07 19:34:46 +0300

    Update

--- functions/message.js
+++ functions/message.js
@@ -11,35 +11,37 @@ exports.handler = async function(event, context) {
         if (event.httpMethod === 'OPTIONS') {
             return { statusCode: '204', headers }
         }
-        const body = JSON.parse(event.body)
-        var mailOptions = {
-            from: process.env.EMAIL,
-            to: 'kingsmilimo@yahoo.com',
-            subject: `New message from ${body.name}`,
-            text: `Message:\n ${body.message}\n\n Phone Number: ${body.phone_number} Email: ${body.email}`,
-            replyTo: `${body.email}`
-        }
-        await transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return {
-                    statusCode: 500,
-                    headers,
-                    body: JSON.stringify({
-                        message: 'Failed',
-                    })
-                    
-                }
-            } else {
-                return {
-                    statusCode: 200,
-                    headers,
-                    body: {
-                        message: 'success',
-                    }
-                    
-                }
+        if (event.httpMethod === 'POST') {
+            const body = JSON.parse(event.body)
+            var mailOptions = {
+                from: process.env.EMAIL,
+                to: 'kingsmilimo@yahoo.com',
+                subject: `New message from ${body.name}`,
+                text: `Message:\n ${body.message}\n\n Phone Number: ${body.phone_number} Email: ${body.email}`,
+                replyTo: `${body.email}`
             }
-        })
+            await transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return {
+                        statusCode: 500,
+                        headers,
+                        body: JSON.stringify({
+                            message: 'Failed',
+                        })
+                        
+                    }
+                } else {
+                    return {
+                        statusCode: 200,
+                        headers,
+                        body: {
+                            message: 'success',
+                        }
+                        
+                    }
+                }
+            })
+        }
     } catch (error) {
         console.log(error)
         return {
